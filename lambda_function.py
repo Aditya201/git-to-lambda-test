9fa0d0681ece746b9337842cbc684468e17e81b2
Shiladitya Khatua <shiladitya.khatua19@gmail.com>
2021-12-27 20:38:06 +0000

    Edited lambda

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1':[1,2], 'col2':[3,4]}
+    d = {'col1':[1,2], 'col2':[3,5]}
     df = pd.DataFrame(d)
     print(df)
     print('Done x1.1')
